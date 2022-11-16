c658f14573d14b81f49c9c6e45c7ff50652f19f8
Kasper Jeppesen <44263766+KasperHJeppesen@users.noreply.github.com>
2022-11-16 15:42:08 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,41 +1,40 @@
-from unicodedata import decimal
-
 #Print start text
-starttext = 'Now, this is a calculator. You need to enter your values'
+starttext = 'This is a calculator.'
 print(starttext)
 
 #Promt the user for two inputs
-values_one = float(input('enter your number: '))
-values_two = float(input('enter your number: '))
+values_one = float(input('Enter your first value: '))
+values_two = float(input('Enter your second value: '))
 
 #We need the + or -
 while True: 
-    sign = input('Enter your sign')
+    sign = input('Enter your sign ')
 
     if sign in ('+', '-'):
         break
 
 #Calculate 
 if sign == '+':
-    c = values_first + values_second
+    c = values_one + values_two
     print("The value with multiple is", c)
 else: 
-    c = values_first - values_second
+    c = values_one - values_two
     print("The value with minus is", c)
 
 #Four calculator options
 def calculate(t1, t2):
+    
     multiple = t1 + t2 
-    print("Multiple gives the value:", multiple)
+    print("Multiple gives the value:", round(multiple, 2))
     
     minus = t1 - t2
-    print("Minus gives the value:", minus)
+    print("Minus gives the value:", round(minus, 2))
     
     add = t1 * t2
-    print("Add gives the value:", add)
+    print("Add gives the value:", round(add, 2))
     
     devide = t1 / t2
-    print("devide gives the value:", devide)
+    print("Devide gives the value:", round(devide, 2))
 
 calculate(values_one, values_two)
 
